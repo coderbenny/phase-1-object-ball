fffa25cadfa8686f901f2a7472d7d02ad065a635
bennyhinnmathew <benny.mathew@student.moringaschool.com>
2023-10-24 12:52:50 +0300

    Completed lab

--- src/01-simple-debug.js
+++ src/01-simple-debug.js
@@ -1,6 +1,14 @@
-console.log('Simple debugging example running.')
-debugger
+// console.log('Simple debugging example running.')
+// debugger
+// let x = 99
+// debugger
+// console.log(x)
 
-let x = 99
-debugger
-console.log(x)
+
+let oo = { foo: 42, bar: 83, "key w/ spaces": true };
+// console.log(oo["key w/ spaces"])
+for (let key in oo){
+    let value = oo[key];
+    console.log("key:", key, "value:", value)
+}
+
--- src/02-advanced-debug.js
+++ src/02-advanced-debug.js
@@ -1,28 +1,93 @@
-console.log('Advanced debugging example running.')
-debugger
 
-// first, define the function.
-function goodPractices() {
-  let game = gameObject();
-  for (let gameKey in game) {
-    // are you ABSOLUTELY SURE what 'gameKey' is?
-    // use the debugger to find out!
-    debugger
-    let teamObj = game[gameKey]
-    for (let teamKey in teamObj) {
-      // are you ABSOLUTELY SURE what 'teamKey' is?
-      // use debugger to find out!
-      debugger
+function numPointsScored(player){
+  const game = gameObject();
 
-      // what is 'data' at each loop through out this block?
-      // when will the following line of code work and when will it break?
-      let data = teamObj.player
-      for (let key in data) {
-        debugger
-      }
+  for (const teamKey in game){
+    if (game[teamKey].players[player]){
+      return game[teamKey].players[player].points
+    }
+  }
+  return `${player} not found`;
+}
+
+function shoeSize(player){
+  const game = gameObject()
+
+  for (const teamKey in game){
+    if (game[teamKey].players[player]){
+      return game[teamKey].players[player].shoe
+    }
+  }
+  return `${player} not found`
+}
+
+function teamColors(teamName){
+  const game = gameObject()
+
+  for (const teamKey in game){
+    if (game[teamKey].teamName){
+      return game[teamKey].colors
+    }
+  }
+  return `${teamName} not found!`
+}
+
+function teamNames(){
+  const game = gameObject()
+  const teams = [];
+
+  for (const teamKey in game){
+    teams.push(game[teamKey].teamName)
+  }
+  return teams
+}
+
+function playerNumbers(teamName){
+  const game = gameObject()
+  
+  for (const teamKey in game){
+    if (game[teamKey].teamName === teamName){
+      const team = game[teamKey]
+      const jerseyNumbers = [];
+
+      for (const playerName in team.players){
+        jerseyNumbers.push(team.players[playerName].number)
+      } 
+      return jerseyNumbers
     }
   }
 }
 
-// then, call the function so it runs!
-goodPractices()
+function playerStats(playerName){
+  const game = gameObject();
+
+  for (const teamKey in game){
+    const player = game[teamKey].players[playerName];
+    if (player){
+      return player
+    }
+  }
+  return `${playerName} not found!`
+}
+function bigShoeRebounds(){
+  const game = gameObject();
+  let playerWithLargestSize = null;
+  let largestShoeSize = 0;
+
+  for (const teamKey in game){
+    const team = game[teamKey];
+    for (const playerName in team.players){
+      const player = team.players[playerName];
+      if (player.shoe > largestShoeSize) {
+        largestShoeSize = player.shoe;
+        playerWithLargestSize = player;
+      }
+    }
+  }
+
+  if (playerWithLargestSize){
+    return playerWithLargestSize.rebounds
+  } else {
+    return "No player found with the largest shoe size."
+  }
+}
